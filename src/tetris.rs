e82a6f9b52bb5e99b3a3871139d6fee3911e023d
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-07-05 04:25:44 -0500

    added soft drop + a bug

--- src/tetris.rs
+++ src/tetris.rs
@@ -7,7 +7,6 @@ use rand::{thread_rng, Rng};
 
 const WIDTH: u8 = 10;
 const HEIGHT: u8 = 22;
-//const VISIBLE_HEIGHT: u8 = 20;
 const GRID_SIZE: f32 = 30.;
 
 enum GamePhase {
@@ -17,7 +16,7 @@ enum GamePhase {
 #[derive(Copy, Clone)]
 struct PieceState {
     index: u8,
-    offset_row: u8,
+    offset_row: i16,
     offset_col: i16,
     rot: u8,
 }
@@ -26,8 +25,8 @@ impl PieceState {
     fn new() -> Self {
         PieceState {
             index: thread_rng().gen_range(0..7),
-            offset_row: 0,
-            offset_col: 0,
+            offset_row: -3,
+            offset_col: 4,
             rot: 0,
         }
     }
@@ -54,7 +53,34 @@ impl Tetris {
         }
     }
 
+    fn merge_piece(&mut self) {
+        let tetromino = TETROMINOS[self.piece.index as usize];
+        for row in 0..tetromino.sides {
+            for col in 0..tetromino.sides {
+                let value = tetromino.get_value(row, col, self.piece.rot);
+
+                if value > 0 {
+                    let row = row as i16 + self.piece.offset_row;
+                    let col = col as i16 + self.piece.offset_col;
+                    self.board[(row * WIDTH as i16 + col) as usize] = value;
+                }
+            }
+        }
+    }
+
+    fn soft_drop(&mut self) {
+        self.piece.offset_row += 1;
+
+        if !self.check_piece_valid(self.piece) {
+            self.piece.offset_row -= 1;
+            self.merge_piece();
+            self.piece = PieceState::new();
+        }
+    }
+
     fn update_game_play(&mut self) {
+        self.soft_drop();
+
         let mut piece = self.piece;
 
         if is_key_pressed(KeyCode::Left) {
@@ -76,11 +102,13 @@ impl Tetris {
         }
     }
 
-    fn board_value(&self, row: u8, col: i16) -> u8 {
-        let x = (row * WIDTH) as i16 + col;
+    fn board_value(&self, row: i16, col: i16) -> u8 {
+        let x = (row * WIDTH as i16) as i16 + col;
+
         if x < 0 {
             return 0;
         }
+
         self.board[x as usize]
     }
 
@@ -91,10 +119,10 @@ impl Tetris {
                 let value = tetromino.get_value(row, col, piece.rot);
 
                 if value > 0 {
-                    let board_row = piece.offset_row + row;
+                    let board_row = piece.offset_row + row as i16;
                     let board_col = piece.offset_col + col as i16;
 
-                    if board_row >= HEIGHT {
+                    if board_row >= HEIGHT as i16 {
                         return false;
                     }
                     if board_col >= WIDTH as i16 {
@@ -113,20 +141,15 @@ impl Tetris {
         true
     }
 
-    fn draw_cell(row: u8, col: i16, value: u8, offset_x: u8, offset_y: u8) {
-        let row = row as f32;
-        let col = col as f32;
-        let offset_x = offset_x as f32;
-        let offset_y = offset_y as f32;
-
+    fn draw_cell(row: i16, col: i16, value: u8) {
         let base_color = BASE_COLORS[value as usize];
         let light_color = LIGHT_COLORS[value as usize];
         let dark_color = DARK_COLORS[value as usize];
 
         let edge = GRID_SIZE / 8.;
 
-        let x = col * GRID_SIZE + offset_x;
-        let y = row * GRID_SIZE + offset_y;
+        let x = col as f32 * GRID_SIZE;
+        let y = row as f32 * GRID_SIZE;
 
         draw_rectangle(x, y, GRID_SIZE, GRID_SIZE, dark_color);
         draw_rectangle(x + edge, y, GRID_SIZE - edge, GRID_SIZE - edge, light_color);
@@ -139,25 +162,35 @@ impl Tetris {
         );
     }
 
-    fn draw_piece(&self, offset_x: u8, offset_y: u8) {
+    fn draw_piece(&self) {
         let tetromino = TETROMINOS[self.piece.index as usize];
         for row in 0..tetromino.sides {
             for col in 0..tetromino.sides {
                 let value = tetromino.get_value(row, col, self.piece.rot);
                 if value > 0 {
                     Tetris::draw_cell(
-                        row + self.piece.offset_row,
+                        row as i16 + self.piece.offset_row,
                         col as i16 + self.piece.offset_col,
                         value,
-                        offset_x,
-                        offset_y,
                     );
                 }
             }
         }
     }
 
+    fn draw_board(&self) {
+        for row in 0..HEIGHT {
+            for col in 0..WIDTH {
+                let value = self.board_value(row as i16, col as i16);
+                if value > 0 {
+                    Tetris::draw_cell(row as i16, col as i16, value);
+                }
+            }
+        }
+    }
+
     pub fn render_game(&mut self) {
-        self.draw_piece(0, 0);
+        self.draw_piece();
+        self.draw_board();
     }
 }
